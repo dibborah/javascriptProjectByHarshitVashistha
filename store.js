d8cb29f202b7eadb3cc557a6fcc620e3c6f40d66
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-31 15:18:49 +0530

    done rendering all the items of store.js on UI

--- store.js
+++ store.js
@@ -2,17 +2,17 @@ const store = {
     todos: [
         {
             id: '1',
-            title: 'Todo 1',
+            title: 'Js Mastery 1',
             checked: false
         },
         {
             id: '2',
-            title: 'Todo 2',
+            title: 'Zustand Tutorial',
             checked: false
         },
         {
             id: '3',
-            title: 'Todo 3',
+            title: 'Todo list project',
             checked: false
         },
     ]
